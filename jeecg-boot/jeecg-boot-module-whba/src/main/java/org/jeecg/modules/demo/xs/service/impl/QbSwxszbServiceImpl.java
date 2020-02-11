07c8b4ca0f662642bd18e98ee2c1397a1ce018d4
liudonghua123 <liudonghua123@gmail.com>
2020-02-11 22:00:22 +0800

    auto setFjscsj

--- jeecg-boot/jeecg-boot-module-whba/src/main/java/org/jeecg/modules/demo/xs/service/impl/QbSwxszbServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-whba/src/main/java/org/jeecg/modules/demo/xs/service/impl/QbSwxszbServiceImpl.java
@@ -12,29 +12,33 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Collection;
+import java.util.Date;
 
 /**
  * @Description: 线索
  * @Author: jeecg-boot
- * @Date:   2020-02-11
+ * @Date: 2020-02-11
  * @Version: V1.0
  */
 @Service
-public class QbSwxszbServiceImpl extends ServiceImpl<QbSwxszbMapper, QbSwxszb> implements IQbSwxszbService {
+public class QbSwxszbServiceImpl extends ServiceImpl<QbSwxszbMapper, QbSwxszb>
+		implements IQbSwxszbService {
 
 	@Autowired
 	private QbSwxszbMapper qbSwxszbMapper;
 	@Autowired
 	private QbSwxszbfjMapper qbSwxszbfjMapper;
-	
+
 	@Override
 	@Transactional
 	public void saveMain(QbSwxszb qbSwxszb, List<QbSwxszbfj> qbSwxszbfjList) {
 		qbSwxszbMapper.insert(qbSwxszb);
-		if(qbSwxszbfjList!=null && qbSwxszbfjList.size()>0) {
-			for(QbSwxszbfj entity:qbSwxszbfjList) {
-				//外键设置
-				entity.setSwxsbh(qbSwxszb.getSwxsbh());
+		if (qbSwxszbfjList != null && qbSwxszbfjList.size() > 0) {
+			for (QbSwxszbfj entity : qbSwxszbfjList) {
+				// 设置上传时间
+				entity.setFjscsj(new Date());
+				// 外键设置
+				entity.setSwxsbh(qbSwxszb.getId());
 				qbSwxszbfjMapper.insert(entity);
 			}
 		}
@@ -42,17 +46,19 @@ public class QbSwxszbServiceImpl extends ServiceImpl<QbSwxszbMapper, QbSwxszb> i
 
 	@Override
 	@Transactional
-	public void updateMain(QbSwxszb qbSwxszb,List<QbSwxszbfj> qbSwxszbfjList) {
+	public void updateMain(QbSwxszb qbSwxszb, List<QbSwxszbfj> qbSwxszbfjList) {
 		qbSwxszbMapper.updateById(qbSwxszb);
-		
-		//1.先删除子表数据
+
+		// 1.先删除子表数据
 		qbSwxszbfjMapper.deleteByMainId(qbSwxszb.getId());
-		
-		//2.子表数据重新插入
-		if(qbSwxszbfjList!=null && qbSwxszbfjList.size()>0) {
-			for(QbSwxszbfj entity:qbSwxszbfjList) {
-				//外键设置
-				entity.setSwxsbh(qbSwxszb.getSwxsbh());
+
+		// 2.子表数据重新插入
+		if (qbSwxszbfjList != null && qbSwxszbfjList.size() > 0) {
+			for (QbSwxszbfj entity : qbSwxszbfjList) {
+				// 设置上传时间
+				entity.setFjscsj(new Date());
+				// 外键设置
+				entity.setSwxsbh(qbSwxszb.getId());
 				qbSwxszbfjMapper.insert(entity);
 			}
 		}
@@ -68,10 +74,10 @@ public class QbSwxszbServiceImpl extends ServiceImpl<QbSwxszbMapper, QbSwxszb> i
 	@Override
 	@Transactional
 	public void delBatchMain(Collection<? extends Serializable> idList) {
-		for(Serializable id:idList) {
+		for (Serializable id : idList) {
 			qbSwxszbfjMapper.deleteByMainId(id.toString());
 			qbSwxszbMapper.deleteById(id);
 		}
 	}
-	
+
 }

7ef5ace024ce5b991fdc633ea81e38fb5005d117
liudonghua123 <liudonghua123@gmail.com>
2020-02-11 22:00:39 +0800

    add customize columns

--- ant-design-vue-jeecg/src/views/xs/QbSwxszbList.vue
+++ ant-design-vue-jeecg/src/views/xs/QbSwxszbList.vue
@@ -17,39 +17,83 @@
           <a-col :md="6" :sm="8">
             <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
               <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
-              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+              <a-button
+                type="primary"
+                @click="searchReset"
+                icon="reload"
+                style="margin-left: 8px"
+              >重置</a-button>
               <a @click="handleToggleSearch" style="margin-left: 8px">
                 {{ toggleSearchStatus ? '收起' : '展开' }}
-                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
+                <a-icon :type="toggleSearchStatus ? 'up' : 'down'" />
               </a>
             </span>
           </a-col>
-
         </a-row>
       </a-form>
     </div>
     <!-- 查询区域-END -->
-    
+
     <!-- 操作按钮区域 -->
     <div class="table-operator">
       <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
       <a-button type="primary" icon="download" @click="handleExportXls('线索')">导出</a-button>
-      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+      <a-upload
+        name="file"
+        :showUploadList="false"
+        :multiple="false"
+        :headers="tokenHeader"
+        :action="importExcelUrl"
+        @change="handleImportExcel"
+      >
         <a-button type="primary" icon="import">导入</a-button>
       </a-upload>
       <a-dropdown v-if="selectedRowKeys.length > 0">
         <a-menu slot="overlay">
-          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+          <a-menu-item key="1" @click="batchDel">
+            <a-icon type="delete" />删除
+          </a-menu-item>
         </a-menu>
-        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+        <a-button style="margin-left: 8px">
+          批量操作
+          <a-icon type="down" />
+        </a-button>
       </a-dropdown>
     </div>
 
     <!-- table区域-begin -->
     <div>
       <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
-        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择
+        <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
         <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+        <a-popover
+          style="float: right; margin-right: 24px"
+          title="自定义列"
+          trigger="click"
+          placement="leftBottom"
+        >
+          <template slot="content">
+            <a-checkbox-group
+              @change="onColSettingsChange"
+              v-model="settingColumns"
+              :defaultValue="settingColumns"
+            >
+              <a-row>
+                <template v-for="(item,index) in defColumns">
+                  <template v-if="item.key!='rowIndex'&& item.dataIndex!='action'">
+                    <a-col :span="12" :key="index">
+                      <a-checkbox :value="item.dataIndex">{{ item.title }}</a-checkbox>
+                    </a-col>
+                  </template>
+                </template>
+              </a-row>
+            </a-checkbox-group>
+          </template>
+          <a>
+            <a-icon type="setting" />自定义列
+          </a>
+        </a-popover>
       </div>
 
       <a-table
@@ -62,14 +106,20 @@
         :pagination="ipagination"
         :loading="loading"
         :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
-        @change="handleTableChange">
-
+        @change="handleTableChange"
+      >
         <template slot="htmlSlot" slot-scope="text">
           <div v-html="text"></div>
         </template>
         <template slot="imgSlot" slot-scope="text">
           <span v-if="!text" style="font-size: 12px;font-style: italic;">无此图片</span>
-          <img v-else :src="getImgView(text)" height="25px" alt="图片不存在" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+          <img
+            v-else
+            :src="getImgView(text)"
+            height="25px"
+            alt="图片不存在"
+            style="max-width:80px;font-size: 12px;font-style: italic;"
+          />
         </template>
         <template slot="fileSlot" slot-scope="text">
           <span v-if="!text" style="font-size: 12px;font-style: italic;">无此文件</span>
@@ -79,9 +129,8 @@
             type="primary"
             icon="download"
             size="small"
-            @click="uploadFile(text)">
-            下载
-          </a-button>
+            @click="uploadFile(text)"
+          >下载</a-button>
         </template>
 
         <span slot="action" slot-scope="text, record">
@@ -89,7 +138,10 @@
 
           <a-divider type="vertical" />
           <a-dropdown>
-            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+            <a class="ant-dropdown-link">
+              更多
+              <a-icon type="down" />
+            </a>
             <a-menu slot="overlay">
               <a-menu-item>
                 <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
@@ -99,7 +151,6 @@
             </a-menu>
           </a-dropdown>
         </span>
-
       </a-table>
     </div>
 
@@ -108,138 +159,133 @@
 </template>
 
 <script>
+import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+import QbSwxszbModal from './modules/QbSwxszbModal'
 
-  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
-  import QbSwxszbModal from './modules/QbSwxszbModal'
-
-  export default {
-    name: "QbSwxszbList",
-    mixins:[JeecgListMixin],
-    components: {
-      QbSwxszbModal
-    },
-    data () {
-      return {
-        description: '线索管理页面',
-        // 表头
-        columns: [
-          {
-            title: '#',
-            dataIndex: '',
-            key:'rowIndex',
-            width:60,
-            align:"center",
-            customRender:function (t,r,index) {
-              return parseInt(index)+1;
-            }
-          },
-          {
-            title:'线索标题',
-            align:"center",
-            dataIndex: 'xsbt'
-          },
-          {
-            title:'线索详情',
-            align:"center",
-            dataIndex: 'xsxq'
-          },
-          {
-            title:'地址编号',
-            align:"center",
-            dataIndex: 'xsddbh'
-          },
-          {
-            title:'地址名称',
-            align:"center",
-            dataIndex: 'xsddmc'
-          },
-          {
-            title:'采集时间',
-            align:"center",
-            dataIndex: 'cjsj',
-            customRender:function (text) {
-              return !text?"":(text.length>10?text.substr(0,10):text)
-            }
-          },
-          {
-            title:'上传时间',
-            align:"center",
-            dataIndex: 'scsj',
-            customRender:function (text) {
-              return !text?"":(text.length>10?text.substr(0,10):text)
-            }
-          },
-          {
-            title:'采集人编号',
-            align:"center",
-            dataIndex: 'cjrbh'
-          },
-          {
-            title:'采集部门编号',
-            align:"center",
-            dataIndex: 'cjbmbh'
-          },
-          {
-            title:'线索类型',
-            align:"center",
-            dataIndex: 'xslx'
-          },
-          {
-            title:'后台计算结果：危险等级',
-            align:"center",
-            dataIndex: 'wxdj'
-          },
-          {
-            title:'后台计算结果：提示详情',
-            align:"center",
-            dataIndex: 'tsxq'
-          },
-          {
-            title:'后台比对标记：Y是，N否',
-            align:"center",
-            dataIndex: 'htbdbj'
-          },
-          {
-            title:'涉稳事件编号',
-            align:"center",
-            dataIndex: 'swsjbh'
-          },
-          {
-            title:'信息编号',
-            align:"center",
-            dataIndex: 'zdasjqbxxbh'
-          },
-          {
-            title: '操作',
-            dataIndex: 'action',
-            align:"center",
-            scopedSlots: { customRender: 'action' },
+export default {
+  name: 'QbSwxszbList',
+  mixins: [JeecgListMixin],
+  components: {
+    QbSwxszbModal
+  },
+  data() {
+    return {
+      description: '线索管理页面',
+      // 表头
+      defColumns: [
+        {
+          title: '#',
+          dataIndex: '',
+          key: 'rowIndex',
+          width: 60,
+          align: 'center',
+          customRender: function(t, r, index) {
+            return parseInt(index) + 1
           }
-        ],
-        url: {
-          list: "/xs/qbSwxszb/list",
-          delete: "/xs/qbSwxszb/delete",
-          deleteBatch: "/xs/qbSwxszb/deleteBatch",
-          exportXlsUrl: "/xs/qbSwxszb/exportXls",
-          importExcelUrl: "xs/qbSwxszb/importExcel",
         },
-        dictOptions:{
+        {
+          title: '线索标题',
+          align: 'center',
+          dataIndex: 'xsbt'
         },
-
-      }
-    },
-    computed: {
-      importExcelUrl: function(){
-        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
-      }
-    },
-    methods: {
-      initDictConfig(){
-      }
-       
+        {
+          title: '线索详情',
+          align: 'center',
+          dataIndex: 'xsxq'
+        },
+        {
+          title: '地址编号',
+          align: 'center',
+          dataIndex: 'xsddbh'
+        },
+        {
+          title: '地址名称',
+          align: 'center',
+          dataIndex: 'xsddmc'
+        },
+        {
+          title: '采集时间',
+          align: 'center',
+          dataIndex: 'cjsj',
+          customRender: function(text) {
+            return !text ? '' : text.length > 10 ? text.substr(0, 10) : text
+          }
+        },
+        {
+          title: '上传时间',
+          align: 'center',
+          dataIndex: 'scsj',
+          customRender: function(text) {
+            return !text ? '' : text.length > 10 ? text.substr(0, 10) : text
+          }
+        },
+        {
+          title: '采集人编号',
+          align: 'center',
+          dataIndex: 'cjrbh'
+        },
+        {
+          title: '采集部门编号',
+          align: 'center',
+          dataIndex: 'cjbmbh'
+        },
+        {
+          title: '线索类型',
+          align: 'center',
+          dataIndex: 'xslx'
+        },
+        {
+          title: '后台计算结果：危险等级',
+          align: 'center',
+          dataIndex: 'wxdj'
+        },
+        {
+          title: '后台计算结果：提示详情',
+          align: 'center',
+          dataIndex: 'tsxq'
+        },
+        {
+          title: '后台比对标记：Y是，N否',
+          align: 'center',
+          dataIndex: 'htbdbj'
+        },
+        {
+          title: '涉稳事件编号',
+          align: 'center',
+          dataIndex: 'swsjbh'
+        },
+        {
+          title: '信息编号',
+          align: 'center',
+          dataIndex: 'zdasjqbxxbh'
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          align: 'center',
+          scopedSlots: { customRender: 'action' }
+        }
+      ],
+      url: {
+        list: '/xs/qbSwxszb/list',
+        delete: '/xs/qbSwxszb/delete',
+        deleteBatch: '/xs/qbSwxszb/deleteBatch',
+        exportXlsUrl: '/xs/qbSwxszb/exportXls',
+        importExcelUrl: 'xs/qbSwxszb/importExcel'
+      },
+      dictOptions: {}
     }
+  },
+  computed: {
+    importExcelUrl: function() {
+      return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`
+    }
+  },
+  methods: {
+    initDictConfig() {}
   }
+}
 </script>
 <style scoped>
-  @import '~@assets/less/common.less'
+@import '~@assets/less/common.less';
 </style>

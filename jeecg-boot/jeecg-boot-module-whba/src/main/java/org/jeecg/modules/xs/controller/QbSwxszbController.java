10f88556ff71231a0bdd0a4f5091dfc2b946cc78
dingqianglong <dingqianglong@163.com>
2020-05-08 16:09:32 +0800

    xszb update for add fill fmmtlj

--- jeecg-boot/jeecg-boot-module-whba/src/main/java/org/jeecg/modules/xs/controller/QbSwxszbController.java
+++ jeecg-boot/jeecg-boot-module-whba/src/main/java/org/jeecg/modules/xs/controller/QbSwxszbController.java
@@ -38,17 +38,19 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import com.alibaba.fastjson.JSON;
+
+import io.netty.util.internal.StringUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.jeecg.common.aspect.annotation.AutoLog;
 
- /**
+/**
  * @Description: 线索
  * @Author: jeecg-boot
- * @Date:   2020-02-26
+ * @Date: 2020-02-26
  * @Version: V1.0
  */
-@Api(tags="线索")
+@Api(tags = "线索")
 @RestController
 @RequestMapping("/xs/qbSwxszb")
 @Slf4j
@@ -57,7 +59,7 @@ public class QbSwxszbController {
 	private IQbSwxszbService qbSwxszbService;
 	@Autowired
 	private IQbSwxszbfjService qbSwxszbfjService;
-	
+
 	/**
 	 * 分页列表查询
 	 *
@@ -68,18 +70,16 @@ public class QbSwxszbController {
 	 * @return
 	 */
 	@AutoLog(value = "线索-分页列表查询")
-	@ApiOperation(value="线索-分页列表查询", notes="线索-分页列表查询")
+	@ApiOperation(value = "线索-分页列表查询", notes = "线索-分页列表查询")
 	@GetMapping(value = "/list")
-	public Result<?> queryPageList(QbSwxszb qbSwxszb,
-								   @RequestParam(name="pageNo", defaultValue="1") Integer pageNo,
-								   @RequestParam(name="pageSize", defaultValue="10") Integer pageSize,
-								   HttpServletRequest req) {
+	public Result<?> queryPageList(QbSwxszb qbSwxszb, @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
+			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize, HttpServletRequest req) {
 		QueryWrapper<QbSwxszb> queryWrapper = QueryGenerator.initQueryWrapper(qbSwxszb, req.getParameterMap());
 		Page<QbSwxszb> page = new Page<QbSwxszb>(pageNo, pageSize);
 		IPage<QbSwxszb> pageList = qbSwxszbService.page(page, queryWrapper);
 		return Result.ok(pageList);
 	}
-	
+
 	/**
 	 * 根据rwid分页列表查询
 	 *
@@ -94,79 +94,88 @@ public class QbSwxszbController {
 	@GetMapping(value = "/listByRwid")
 	public Result<?> queryPageListByRwid(@RequestParam(name = "rwid") String rwid,
 			@RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
-			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize,
-			HttpServletRequest req) {
+			@RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize, HttpServletRequest req) {
 		QueryWrapper<QbSwxszb> queryWrapper = new QueryWrapper<>();
 		queryWrapper.eq("rwid", rwid);
 		Page<QbSwxszb> page = new Page<QbSwxszb>(pageNo, pageSize);
 		IPage<QbSwxszb> pageList = qbSwxszbService.page(page, queryWrapper);
 		return Result.ok(pageList);
 	}
-	
+
 	/**
-	 *   添加
+	 * 添加
 	 *
 	 * @param qbSwxszbPage
 	 * @return
 	 */
 	@AutoLog(value = "线索-添加")
-	@ApiOperation(value="线索-添加", notes="线索-添加")
+	@ApiOperation(value = "线索-添加", notes = "线索-添加")
 	@PostMapping(value = "/add")
 	public Result<?> add(@RequestBody QbSwxszbPage qbSwxszbPage) {
 		QbSwxszb qbSwxszb = new QbSwxszb();
 		BeanUtils.copyProperties(qbSwxszbPage, qbSwxszb);
+		fillQbSwxszbFmmtlj(qbSwxszb, qbSwxszbPage);
 		qbSwxszbService.saveMain(qbSwxszb, qbSwxszbPage.getQbSwxszbfjList());
 		return Result.ok("添加成功！");
 	}
-	
+
+	private void fillQbSwxszbFmmtlj(QbSwxszb qbSwxszb, QbSwxszbPage qbSwxszbPage) {
+		if (qbSwxszb != null && StringUtil.isNullOrEmpty(qbSwxszb.getFmmtlj())) {
+			if (qbSwxszbPage.getQbSwxszbfjList() != null && qbSwxszbPage.getQbSwxszbfjList().size() > 0) {
+				qbSwxszb.setFmmtlj(qbSwxszbPage.getQbSwxszbfjList().get(0).getWjlj());
+			}
+		}
+	}
+
 	/**
-	 *  编辑
+	 * 编辑
 	 *
 	 * @param qbSwxszbPage
 	 * @return
 	 */
 	@AutoLog(value = "线索-编辑")
-	@ApiOperation(value="线索-编辑", notes="线索-编辑")
+	@ApiOperation(value = "线索-编辑", notes = "线索-编辑")
 	@PutMapping(value = "/edit")
 	public Result<?> edit(@RequestBody QbSwxszbPage qbSwxszbPage) {
 		QbSwxszb qbSwxszb = new QbSwxszb();
 		BeanUtils.copyProperties(qbSwxszbPage, qbSwxszb);
+		fillQbSwxszbFmmtlj(qbSwxszb, qbSwxszbPage);
 		QbSwxszb qbSwxszbEntity = qbSwxszbService.getById(qbSwxszb.getId());
-		if(qbSwxszbEntity==null) {
+		if (qbSwxszbEntity == null) {
 			return Result.error("未找到对应数据");
 		}
 		qbSwxszbService.updateMain(qbSwxszb, qbSwxszbPage.getQbSwxszbfjList());
 		return Result.ok("编辑成功!");
 	}
-	
+
 	/**
-	 *   通过id删除
+	 * 通过id删除
 	 *
 	 * @param id
 	 * @return
 	 */
 	@AutoLog(value = "线索-通过id删除")
-	@ApiOperation(value="线索-通过id删除", notes="线索-通过id删除")
+	@ApiOperation(value = "线索-通过id删除", notes = "线索-通过id删除")
 	@DeleteMapping(value = "/delete")
-	public Result<?> delete(@RequestParam(name="id",required=true) String id) {
+	public Result<?> delete(@RequestParam(name = "id", required = true) String id) {
 		qbSwxszbService.delMain(id);
 		return Result.ok("删除成功!");
 	}
-	
+
 	/**
-	 *  批量删除
+	 * 批量删除
 	 *
 	 * @param ids
 	 * @return
 	 */
 	@AutoLog(value = "线索-批量删除")
-	@ApiOperation(value="线索-批量删除", notes="线索-批量删除")
+	@ApiOperation(value = "线索-批量删除", notes = "线索-批量删除")
 	@DeleteMapping(value = "/deleteBatch")
-	public Result<?> deleteBatch(@RequestParam(name="ids",required=true) String ids) {
+	public Result<?> deleteBatch(@RequestParam(name = "ids", required = true) String ids) {
 		this.qbSwxszbService.delBatchMain(Arrays.asList(ids.split(",")));
 		return Result.ok("批量删除成功！");
 	}
-	
+
 	/**
 	 * 通过id查询
 	 *
@@ -174,17 +183,17 @@ public class QbSwxszbController {
 	 * @return
 	 */
 	@AutoLog(value = "线索-通过id查询")
-	@ApiOperation(value="线索-通过id查询", notes="线索-通过id查询")
+	@ApiOperation(value = "线索-通过id查询", notes = "线索-通过id查询")
 	@GetMapping(value = "/queryById")
-	public Result<?> queryById(@RequestParam(name="id",required=true) String id) {
+	public Result<?> queryById(@RequestParam(name = "id", required = true) String id) {
 		QbSwxszb qbSwxszb = qbSwxszbService.getById(id);
-		if(qbSwxszb==null) {
+		if (qbSwxszb == null) {
 			return Result.error("未找到对应数据");
 		}
 		return Result.ok(qbSwxszb);
 
 	}
-	
+
 	/**
 	 * 通过id查询
 	 *
@@ -192,93 +201,94 @@ public class QbSwxszbController {
 	 * @return
 	 */
 	@AutoLog(value = "线索附件集合-通过id查询")
-	@ApiOperation(value="线索附件集合-通过id查询", notes="线索附件-通过id查询")
+	@ApiOperation(value = "线索附件集合-通过id查询", notes = "线索附件-通过id查询")
 	@GetMapping(value = "/queryQbSwxszbfjByMainId")
-	public Result<?> queryQbSwxszbfjListByMainId(@RequestParam(name="id",required=true) String id) {
+	public Result<?> queryQbSwxszbfjListByMainId(@RequestParam(name = "id", required = true) String id) {
 		List<QbSwxszbfj> qbSwxszbfjList = qbSwxszbfjService.selectByMainId(id);
 		return Result.ok(qbSwxszbfjList);
 	}
 
-    /**
-    * 导出excel
-    *
-    * @param request
-    * @param qbSwxszb
-    */
-    @RequestMapping(value = "/exportXls")
-    public ModelAndView exportXls(HttpServletRequest request, QbSwxszb qbSwxszb) {
-      // Step.1 组装查询条件查询数据
-      QueryWrapper<QbSwxszb> queryWrapper = QueryGenerator.initQueryWrapper(qbSwxszb, request.getParameterMap());
-      LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+	/**
+	 * 导出excel
+	 *
+	 * @param request
+	 * @param qbSwxszb
+	 */
+	@RequestMapping(value = "/exportXls")
+	public ModelAndView exportXls(HttpServletRequest request, QbSwxszb qbSwxszb) {
+		// Step.1 组装查询条件查询数据
+		QueryWrapper<QbSwxszb> queryWrapper = QueryGenerator.initQueryWrapper(qbSwxszb, request.getParameterMap());
+		LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
 
-      //Step.2 获取导出数据
-      List<QbSwxszb> queryList = qbSwxszbService.list(queryWrapper);
-      // 过滤选中数据
-      String selections = request.getParameter("selections");
-      List<QbSwxszb> qbSwxszbList = new ArrayList<QbSwxszb>();
-      if(oConvertUtils.isEmpty(selections)) {
-          qbSwxszbList = queryList;
-      }else {
-          List<String> selectionList = Arrays.asList(selections.split(","));
-          qbSwxszbList = queryList.stream().filter(item -> selectionList.contains(item.getId())).collect(Collectors.toList());
-      }
+		// Step.2 获取导出数据
+		List<QbSwxszb> queryList = qbSwxszbService.list(queryWrapper);
+		// 过滤选中数据
+		String selections = request.getParameter("selections");
+		List<QbSwxszb> qbSwxszbList = new ArrayList<QbSwxszb>();
+		if (oConvertUtils.isEmpty(selections)) {
+			qbSwxszbList = queryList;
+		} else {
+			List<String> selectionList = Arrays.asList(selections.split(","));
+			qbSwxszbList = queryList.stream().filter(item -> selectionList.contains(item.getId()))
+					.collect(Collectors.toList());
+		}
 
-      // Step.3 组装pageList
-      List<QbSwxszbPage> pageList = new ArrayList<QbSwxszbPage>();
-      for (QbSwxszb main : qbSwxszbList) {
-          QbSwxszbPage vo = new QbSwxszbPage();
-          BeanUtils.copyProperties(main, vo);
-          List<QbSwxszbfj> qbSwxszbfjList = qbSwxszbfjService.selectByMainId(main.getId());
-          vo.setQbSwxszbfjList(qbSwxszbfjList);
-          pageList.add(vo);
-      }
+		// Step.3 组装pageList
+		List<QbSwxszbPage> pageList = new ArrayList<QbSwxszbPage>();
+		for (QbSwxszb main : qbSwxszbList) {
+			QbSwxszbPage vo = new QbSwxszbPage();
+			BeanUtils.copyProperties(main, vo);
+			List<QbSwxszbfj> qbSwxszbfjList = qbSwxszbfjService.selectByMainId(main.getId());
+			vo.setQbSwxszbfjList(qbSwxszbfjList);
+			pageList.add(vo);
+		}
 
-      // Step.4 AutoPoi 导出Excel
-      ModelAndView mv = new ModelAndView(new JeecgEntityExcelView());
-      mv.addObject(NormalExcelConstants.FILE_NAME, "线索列表");
-      mv.addObject(NormalExcelConstants.CLASS, QbSwxszbPage.class);
-      mv.addObject(NormalExcelConstants.PARAMS, new ExportParams("线索数据", "导出人:"+sysUser.getRealname(), "线索"));
-      mv.addObject(NormalExcelConstants.DATA_LIST, pageList);
-      return mv;
-    }
+		// Step.4 AutoPoi 导出Excel
+		ModelAndView mv = new ModelAndView(new JeecgEntityExcelView());
+		mv.addObject(NormalExcelConstants.FILE_NAME, "线索列表");
+		mv.addObject(NormalExcelConstants.CLASS, QbSwxszbPage.class);
+		mv.addObject(NormalExcelConstants.PARAMS, new ExportParams("线索数据", "导出人:" + sysUser.getRealname(), "线索"));
+		mv.addObject(NormalExcelConstants.DATA_LIST, pageList);
+		return mv;
+	}
 
-    /**
-    * 通过excel导入数据
-    *
-    * @param request
-    * @param response
-    * @return
-    */
-    @RequestMapping(value = "/importExcel", method = RequestMethod.POST)
-    public Result<?> importExcel(HttpServletRequest request, HttpServletResponse response) {
-      MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-      Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
-      for (Map.Entry<String, MultipartFile> entity : fileMap.entrySet()) {
-          MultipartFile file = entity.getValue();// 获取上传文件对象
-          ImportParams params = new ImportParams();
-          params.setTitleRows(2);
-          params.setHeadRows(1);
-          params.setNeedSave(true);
-          try {
-              List<QbSwxszbPage> list = ExcelImportUtil.importExcel(file.getInputStream(), QbSwxszbPage.class, params);
-              for (QbSwxszbPage page : list) {
-                  QbSwxszb po = new QbSwxszb();
-                  BeanUtils.copyProperties(page, po);
-                  qbSwxszbService.saveMain(po, page.getQbSwxszbfjList());
-              }
-              return Result.ok("文件导入成功！数据行数:" + list.size());
-          } catch (Exception e) {
-              log.error(e.getMessage(),e);
-              return Result.error("文件导入失败:"+e.getMessage());
-          } finally {
-              try {
-                  file.getInputStream().close();
-              } catch (IOException e) {
-                  e.printStackTrace();
-              }
-          }
-      }
-      return Result.ok("文件导入失败！");
-    }
+	/**
+	 * 通过excel导入数据
+	 *
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(value = "/importExcel", method = RequestMethod.POST)
+	public Result<?> importExcel(HttpServletRequest request, HttpServletResponse response) {
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+		for (Map.Entry<String, MultipartFile> entity : fileMap.entrySet()) {
+			MultipartFile file = entity.getValue();// 获取上传文件对象
+			ImportParams params = new ImportParams();
+			params.setTitleRows(2);
+			params.setHeadRows(1);
+			params.setNeedSave(true);
+			try {
+				List<QbSwxszbPage> list = ExcelImportUtil.importExcel(file.getInputStream(), QbSwxszbPage.class, params);
+				for (QbSwxszbPage page : list) {
+					QbSwxszb po = new QbSwxszb();
+					BeanUtils.copyProperties(page, po);
+					qbSwxszbService.saveMain(po, page.getQbSwxszbfjList());
+				}
+				return Result.ok("文件导入成功！数据行数:" + list.size());
+			} catch (Exception e) {
+				log.error(e.getMessage(), e);
+				return Result.error("文件导入失败:" + e.getMessage());
+			} finally {
+				try {
+					file.getInputStream().close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return Result.ok("文件导入失败！");
+	}
 
 }
